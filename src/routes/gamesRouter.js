8db99511f3c0d658038758949d4e29381a869015
bielvbcg <gabrielvbcallegario@gmail.com>
2022-02-12 20:29:15 -0300

    add dataBase integration

--- src/routes/gamesRouter.js
+++ src/routes/gamesRouter.js
@@ -1,8 +1,11 @@
 import { Router } from 'express';
-import { getGames } from "../controllers/gamesController.js"
+import { getGames, addGame } from "../controllers/gamesController.js"
 import tokenValidation from '../middlewares/tokenValidation.js'
+import schemaValidation from '../middlewares/schemaValidation.js'
+import gameSchema from '../schemas/gameSchema.js'
 
 const gamesRouter = Router();
 
 gamesRouter.get('/games', getGames)
+gamesRouter.post('/games', schemaValidation(gameSchema), addGame)
 export default gamesRouter;
